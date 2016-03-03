5bc19ac69c21d46c475ff7e0ed9ee3d8d62d1dd0
Web-Push <cloudlearning777@gmail.com>
2016-03-03 17:26:44 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -33,7 +33,41 @@ self.addEventListener('message', function(event) {
     event.waitUntil(promise);
   }
 });
+// Listen for messages from clients.
+self.addEventListener('push', function(event) {
+  // Get all the connected clients and forward the message along.
+  var promise = self.clients.matchAll({includeUncontrolled:true})
+  .then(function(clientList) {
+    // event.source.id contains the ID of the sender of the message.
+    // `event` in Chrome isn't an ExtendableMessageEvent yet (https://slightlyoff.github.io/ServiceWorker/spec/service_worker/#extendablemessage-event-interface),
+    // so it doesn't have the `source` property.
+    // https://code.google.com/p/chromium/issues/detail?id=543198
+    var senderID = event.source ? event.source.id : 'unknown';
 
+    // We'll also print a warning, so users playing with the demo aren't confused.
+    if (!event.source) {
+      console.log('event.source is null; we don\'t know the sender of the ' +
+                  'message');
+    }
+
+    clientList.forEach(function(client) {
+      // Skip sending the message to the client that sent it.
+      if (client.id === senderID) {
+        return;
+      }
+      client.postMessage({
+        client: senderID,
+        message: event.data
+      });
+    });
+  });
+
+  // If event.waitUntil is defined (not yet in Chrome because of the same issue detailed before),
+  // use it to extend the lifetime of the Service Worker.
+  if (event.waitUntil) {
+    event.waitUntil(promise);
+  }
+});
 self.addEventListener('install', function(event) {
   console.log('install');
   event.waitUntil(self.skipWaiting().then(function() {
